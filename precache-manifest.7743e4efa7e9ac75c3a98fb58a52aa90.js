f2912b3c95c2b52fcefa5524a63a15d64d472758
bomotayo <omotayobabatundee@gmail.com>
2020-07-09 14:54:43 -0400

    Updates

---
+++ precache-manifest.7743e4efa7e9ac75c3a98fb58a52aa90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5966abf6e88017b667afb46c12ca83a0",
+    "url": "/restaurants/index.html"
+  },
+  {
+    "revision": "75d68c210c9cab7d6823",
+    "url": "/restaurants/static/css/main.f2751d05.chunk.css"
+  },
+  {
+    "revision": "298b8c85eb869362b913",
+    "url": "/restaurants/static/js/2.1650b099.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/restaurants/static/js/2.1650b099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d68c210c9cab7d6823",
+    "url": "/restaurants/static/js/main.0cef88a7.chunk.js"
+  },
+  {
+    "revision": "3faf666bb07a5b91f48a",
+    "url": "/restaurants/static/js/runtime-main.41f6af45.js"
+  },
+  {
+    "revision": "ecf427f7510fa5366337cefeeeb727d2",
+    "url": "/restaurants/static/media/MR ROBOT.ecf427f7.ttf"
+  }
+]);
